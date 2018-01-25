ae65f4924bb0c10a196adeaa7e696c1dae496593
jeongyeon <jeongyeon@192.168.200.178>
2018-01-25 10:18:54 +0900

    1018

--- www/js/doctor.js
+++ www/js/doctor.js
@@ -67,24 +67,27 @@ function DoctorBoard()
 										{
 											BoardCount++;
 											ForCount++;
-											var insertTXT = '<div class="doctor_notice_contents" id="Board_'+snapshot.key+'" onclick=BoardCaseOpen("'+snapshot.key+'")>'
-																			+'<ul>'
-																				+'<li><div class="doctor_notice_contents_img"><img src="../img/burnkind/junggi/junggi1.png" width="100%"></div></li>'
-																			+'</ul>'
-																			+'<ul>'
-																				+'<li><div class="doctor_notice_contents_burn">전기화상</div></li>'
-																				+'<li><div class="doctor_notice_contents_title">'+snapshot.child('title').val()+'</div></li>'
-																				+'<li><div class="doctor_notice_contents_content">'+BoardCount+'</div></li>'
-																			+'</ul>'
-																			+'<ul>'
-																				+'<li><div class="doctor_notice_contents_detail"><img src="../img/detail_down.png" width="100%"></div></li>'
-																			+'</ul>'
-																		+'</div>'
-																		+'<div class="doctor_notice_date">'
-																			+'<div class="doctor_notice_bool">답변대기중</div><div class="doctor_notice_detail">2018년 1월 11일</div>'
-																		+'</div>'
-																		+'<div id=BoardCase'+snapshot.key+'  style="display:none">'
-																		+'</div>'
+											var insertTXT = 	'<div id="Board_'+snapshot.key+'" onclick=BoardCaseOpen("'+snapshot.key+'")>'
+																				+'<div class="doctor_notice_contents">'
+																				+'<ul>'
+																					+'<li><div class="doctor_notice_contents_img"><img src="../img/burnkind/junggi/junggi1.png" width="100%"></div></li>'
+																				+'</ul>'
+																				+'<ul>'
+																					+'<li><div class="doctor_notice_contents_burn">전기화상</div></li>'
+																					+'<li><div class="doctor_notice_contents_title">'+snapshot.child('title').val()+'</div></li>'
+																					+'<li><div class="doctor_notice_contents_content">'+BoardCount+'</div></li>'
+																				+'</ul>'
+																				+'<ul>'
+																					+'<li><div class="doctor_notice_contents_detail"><img src="../img/detail_down.png" width="100%"></div></li>'
+																				+'</ul>'
+																				+'</div>'
+																				+'<div class="doctor_notice_date">'
+																				+'<div class="doctor_notice_bool">답변대기중</div><div class="doctor_notice_detail">2018년 1월 11일</div>'
+																			+'</div>'	
+																			+'</div>'
+																			+'<div id=BoardCase'+snapshot.key+'  style="display:none">'
+																			+'</div>'
+
 											document.getElementById('doctor_notice_board').insertAdjacentHTML('afterBegin', insertTXT);	
 											
 											if(ForCount==4)
@@ -97,7 +100,35 @@ function DoctorBoard()
 					}
 				else//총 갯수가 4개 이하임
 					{
-					
+					DoctorBoardDB.limitToLast(TotalCount).once('value', function(snap)
+							{
+								snap.forEach(function(snapshot)
+										{
+											BoardCount++;
+											var insertTXT = 	'<div id="Board_'+snapshot.key+'" onclick=BoardCaseOpen("'+snapshot.key+'")>'
+																				+'<div class="doctor_notice_contents">'
+																				+'<ul>'
+																					+'<li><div class="doctor_notice_contents_img"><img src="../img/burnkind/junggi/junggi1.png" width="100%"></div></li>'
+																				+'</ul>'
+																				+'<ul>'
+																					+'<li><div class="doctor_notice_contents_burn">전기화상</div></li>'
+																					+'<li><div class="doctor_notice_contents_title">'+snapshot.child('title').val()+'</div></li>'
+																					+'<li><div class="doctor_notice_contents_content">'+BoardCount+'</div></li>'
+																				+'</ul>'
+																				+'<ul>'
+																					+'<li><div class="doctor_notice_contents_detail"><img src="../img/detail_down.png" width="100%"></div></li>'
+																				+'</ul>'
+																				+'</div>'
+																				+'<div class="doctor_notice_date">'
+																				+'<div class="doctor_notice_bool">답변대기중</div><div class="doctor_notice_detail">2018년 1월 11일</div>'
+																			+'</div>'	
+																			+'</div>'
+																			+'<div id=BoardCase'+snapshot.key+'  style="display:none">'
+																			+'</div>'
+
+											document.getElementById('doctor_notice_board').insertAdjacentHTML('afterBegin', insertTXT);
+										})
+							})
 					}
 			})
 
@@ -125,22 +156,26 @@ function DoctorBoardMore(getCount)
 										{
 											BoardCount++;
 											ForCount++;
-											var insertTXT = '<div class="doctor_notice_contents">'
-																			+'<ul>'
-																				+'<li><div class="doctor_notice_contents_img"><img src="../img/burnkind/junggi/junggi1.png" width="100%"></div></li>'
-																			+'</ul>'
-																			+'<ul>'
-																				+'<li><div class="doctor_notice_contents_burn">전기화상</div></li>'
-																				+'<li><div class="doctor_notice_contents_title">'+snapshot.child('title').val()+'</div></li>'
-																				+'<li><div class="doctor_notice_contents_content">'+BoardCount+'</div></li>'
-																			+'</ul>'
-																			+'<ul>'
-																				+'<li><div class="doctor_notice_contents_detail"><img src="../img/detail_down.png" width="100%"></div></li>'
-																			+'</ul>'
+											var insertTXT = '<div id="Board_'+snapshot.key+'" onclick=BoardCaseOpen("'+snapshot.key+'")>'
+																		+'<div class="doctor_notice_contents">'
+																		+'<ul>'
+																			+'<li><div class="doctor_notice_contents_img"><img src="../img/burnkind/junggi/junggi1.png" width="100%"></div></li>'
+																		+'</ul>'
+																		+'<ul>'
+																			+'<li><div class="doctor_notice_contents_burn">전기화상</div></li>'
+																			+'<li><div class="doctor_notice_contents_title">'+snapshot.child('title').val()+'</div></li>'
+																			+'<li><div class="doctor_notice_contents_content">'+BoardCount+'</div></li>'
+																		+'</ul>'
+																		+'<ul>'
+																			+'<li><div class="doctor_notice_contents_detail"><img src="../img/detail_down.png" width="100%"></div></li>'
+																		+'</ul>'
 																		+'</div>'
 																		+'<div class="doctor_notice_date">'
-																			+'<div class="doctor_notice_bool">답변대기중</div><div class="doctor_notice_detail">2018년 1월 11일</div>'
-																		+'</div>';
+																		+'<div class="doctor_notice_bool">답변대기중</div><div class="doctor_notice_detail">2018년 1월 11일</div>'
+																	+'</div>'	
+																	+'</div>'
+																	+'<div id=BoardCase'+snapshot.key+'  style="display:none">'
+																	+'</div>'
 											document.getElementById('BoardMore'+BoardMoreCount).insertAdjacentHTML('afterBegin', insertTXT);	
 											
 											if(ForCount==4)
@@ -164,22 +199,26 @@ function DoctorBoardMore(getCount)
 											{
 												BoardCount++;
 												ForCount++;
-												var insertTXT = '<div class="doctor_notice_contents">'
-																				+'<ul>'
-																					+'<li><div class="doctor_notice_contents_img"><img src="../img/burnkind/junggi/junggi1.png" width="100%"></div></li>'
-																				+'</ul>'
-																				+'<ul>'
-																					+'<li><div class="doctor_notice_contents_burn">전기화상</div></li>'
-																					+'<li><div class="doctor_notice_contents_title">'+snapshot.child('title').val()+'</div></li>'
-																					+'<li><div class="doctor_notice_contents_content">'+BoardCount+'</div></li>'
-																				+'</ul>'
-																				+'<ul>'
-																					+'<li><div class="doctor_notice_contents_detail"><img src="../img/detail_down.png" width="100%"></div></li>'
-																				+'</ul>'
+												var insertTXT = '<div id="Board_'+snapshot.key+'" onclick=BoardCaseOpen("'+snapshot.key+'")>'
+																			+'<div class="doctor_notice_contents">'
+																			+'<ul>'
+																				+'<li><div class="doctor_notice_contents_img"><img src="../img/burnkind/junggi/junggi1.png" width="100%"></div></li>'
+																			+'</ul>'
+																			+'<ul>'
+																				+'<li><div class="doctor_notice_contents_burn">전기화상</div></li>'
+																				+'<li><div class="doctor_notice_contents_title">'+snapshot.child('title').val()+'</div></li>'
+																				+'<li><div class="doctor_notice_contents_content">'+BoardCount+'</div></li>'
+																			+'</ul>'
+																			+'<ul>'
+																				+'<li><div class="doctor_notice_contents_detail"><img src="../img/detail_down.png" width="100%"></div></li>'
+																			+'</ul>'
 																			+'</div>'
 																			+'<div class="doctor_notice_date">'
-																				+'<div class="doctor_notice_bool">답변대기중</div><div class="doctor_notice_detail">2018년 1월 11일</div>'
-																			+'</div>';
+																			+'<div class="doctor_notice_bool">답변대기중</div><div class="doctor_notice_detail">2018년 1월 11일</div>'
+																		+'</div>'	
+																		+'</div>'
+																		+'<div id=BoardCase'+snapshot.key+'  style="display:none">'
+																		+'</div>'
 												document.getElementById('BoardMore'+BoardMoreCount).insertAdjacentHTML('afterBegin', insertTXT);	
 												
 
@@ -209,9 +248,9 @@ function BoardCaseOpen(getId)
 																	+'imgurl1: '+snapshot.child('imgurl1').val()+'<br>'
 																	+'imgurl2: '+snapshot.child('imgurl2').val()+'<br>'
 																	+'status: '+snapshot.child('status').val()+'<br>'
-																	+'<textarea id=txt_'+snapshot.key+'></textarea>'
-																	+'<button id=btn_'+snapshot.key+'>등록</button>'
-																	+'================='+snapshot.key+'끝====================<br>'
+																	+'<textarea id=txt_'+snap.key+'_'+snapshot.key+'></textarea>'
+																	+'<button id=btn_'+snap.key+'_'+snapshot.key+' onclick=BoardInsert("'+snap.key+'","'+snapshot.key+'")>등록</button>'
+																	+'<br>================='+snapshot.key+'끝====================<br>'
 							document.getElementById('BoardCase'+getId).insertAdjacentHTML('afterBegin', BoardCaseFrame);	
 						})
 			})
@@ -222,5 +261,130 @@ function BoardCaseClose(getId)
 {
 	$('#Board_'+getId).attr('onclick', 'BoardCaseOpen("'+getId+'")');
 	$('#BoardCase'+getId).hide();
+}
+
+
+function BoardInsert(key, casenum)
+{
+	console.log("key: "+key);
+	console.log("casenum: "+casenum);
+	
+	
+	
+	var Seq = key
+	var CaseNum =casenum;
+	var user = firebase.auth().currentUser;
+	var uid = user.uid;
+	var doctorName = user.displayName
+	
+
+	var now = new Date();
+	
+	var year = (now.getYear()+1900).toString(); 
+	var month = (now.getMonth()+1).toString();
+	var day = (now.getDate()).toString();
+	var hour = now.getHours().toString();
+	var min = now.getMinutes().toString();
+	var sec = now.getSeconds().toString();
+	
+	var Fulldate= year+(month[1]? month:'0'+month[0])+(day[1]? day:'0'+day[0])+(hour[1]? hour:'0'+hour[0])+(min[1]? min:'0'+min[0])+(sec[1]? sec:'0'+sec[0]);
+	
+	
+	
+	var AnswerInsertDB = firebase.database().ref().child("Answer/"+Seq+"/"+CaseNum);
+	var ConfirmStatus = firebase.database().ref().child("Question/"+Seq);
+
+	ConfirmStatus.once('value' , function(snapshot)
+			{
+		if(snapshot.numChildren()==0)
+			{
+			swal(
+					  '실패',
+					  '해당 질문이 삭제되었습니다!',
+					  'error'
+					).then(function()
+							{
+								$.mobile.pageContainer.pagecontainer( "change", "answer.html", { transition:"slideup", reverse:true } )
+							})
+			}
+			
+
+		else
+		{
+			if(snapshot.child('prostatus').val()=="Q")
+			{
+			AnswerInsertDB.set({
+				date:Fulldate,
+				uid:uid,
+				contents:$("#txt_"+key+'_'+casenum).val().replace(/\n/g, '<br>'),
+			}).then(function()
+					{
+						QuestionStatusUpdate(Seq, Fulldate, uid, doctorName, casenum)
+					})
+			}
+		else if(snapshot.child('prostatus').val()=="A")
+			{
+			swal(
+					  '실패',
+					  '다른 답변이 먼저 등록되었습니다!',
+					  'error'
+					).then(function()
+							{
+								$.mobile.pageContainer.pagecontainer( "change", "answer.html", { transition:"slideup", reverse:true } )
+							})
+			}
+		}
+			});	
+}
+
+function QuestionStatusUpdate(Seq, Fulldate, uid, doctorName, casenum)
+{
+	var QuestionStatusDB = firebase.database().ref().child('Question/'+Seq);
+	var CaseStatusDB = firebase.database().ref().child('Case/'+Seq+"/"+casenum)
+	
+	
+	console.log(Seq)
+	console.log(casenum)
+	
+	CaseStatusDB.update
+	({
+			status:"A"
+	})
+	QuestionStatusDB.update(
+			{
+				prostatus:"A",
+				answerdoc: uid,
+				prostatus_answerdoc:"A_"+uid
+			}).then(function(){
+				swal({
+       			  title: '성공!',
+       			  text: '해당 글은 나의 답변에서 확인 할 수 있습니다.',
+       			  type: 'success',
+       			  confirmButtonText: '확인'
+       			})
+       						
+       						var splitqnum = Seq.split("_");
+       						var splitedqnum = splitqnum[1];
+       				
+       				
+       						var UserDetailDB = firebase.database().ref().child('User/'+splitedqnum);
+       						
+       						UserDetailDB.once('value', function(snap)
+       								{
+       									var OS = snap.child('OS').val();
+       									var Token = snap.child('Token').val();
+       									var switch2 = snap.child('switch2').val();
+       									console.log("토큰: "+Token);
+       									if(Token!=""&&switch2=="On")
+       										{
+       											var Msg = "회원님의 질문에 "+doctorName+" 님이 답변을 등록하였습니다.";
+       											SendMessageToTarget(Token, Msg);
+       										}
+       									
+       									$('#Board_'+Seq).hide();
+       									$('#BoardCase'+Seq).hide();
+       								})
+       					
+	})
 }
 
